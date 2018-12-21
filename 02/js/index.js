054630465fecaf8f70b920b59e09f8ebeea687a2
yun2 <yun2panda@gmail.com>
2018-12-21 11:02:07 +0900

    init

---
+++ 02/js/index.js
@@ -0,0 +1,28 @@
+(function () {
+    $(function () {
+        setLang('en');
+
+        $('#lang').on('change', function () {
+            var lang = $('#lang').val();
+            setLang(lang);
+        });
+    });
+
+    function setLang(lang) {
+        log('lang=' + lang);
+
+        i18next.use(i18nextXHRBackend).init({
+            backend: {
+                loadPath: 'locales/{{lng}}/string.json'
+            },
+            debug: false,
+            defaultLng: 'en',
+            fallbackLng: false,
+            lng: lang,
+        }, function (err, t) {
+            jqueryI18next.init(i18next, $);
+            $('[data-i18n]').localize();
+        });
+    }
+
+})();
